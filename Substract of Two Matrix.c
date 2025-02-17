2a825adfae584c9f682406737f6fb6b0042493a8
Ketan Singh <hackerrajaput6@gmail.com>
2025-02-18 00:03:34 +0530

    Substract of Two Matrix.c

---
+++ Substract of Two Matrix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void main() {
+  int r, c, A[100][100], B[100][100], C[100][100], i, j;
+  // c = Coloum of Matrix , r = Row of Matrix 
+  printf("Enter the number of rows (between 1 and 100): ");
+  scanf("%d", &r);
+  printf("Enter the number of columns (between 1 and 100): ");
+  scanf("%d", &c);
+
+      // Enter the Matrix A
+  printf("\nEnter elements of 1st matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element a%d%d: ", i + 1, j + 1);
+      scanf("%d", &A[i][j]);
+    }
+
+     // Enter the Matrix B
+  printf("Enter elements of 2nd matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element b%d%d: ", i + 1, j + 1);
+      scanf("%d", &B[i][j]);
+    }
+
+  // substract two matrices
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      C[i][j] = A[i][j] - B[i][j];
+    }
+
+  // printing the result
+  printf("\nSub of two matrices: \n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("%d   ", C[i][j]);
+      if (j == c - 1) {
+        printf("\n\n");
+      }
+    }
+
+  
+}
